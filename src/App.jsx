5a212679bb0da94bc8bc4b349ac62c6e74acc4f1
nachobaez96 <nachobaez96@gmail.com>
2024-05-22 21:40:31 +0200

    First functional version

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import './index.css';
 
+const initialMarkdown = `
+# Heading 1
+## Heading 2
+[Link](https://www.example.com)
+\`Inline code\`
+\`\`\`
+Code block
+\`\`\`
+- List item
+> Blockquote
+![Image](https://via.placeholder.com/150)
+**Bold text**
+`;
 
-function App() {
+export default function App() {
+  const [markdown, setMarkdown] = useState(initialMarkdown);
+
+  const handleChange = (event) => {
+    setMarkdown(event.target.value);
+  };
 
   return (
-      <div>
+    <div id="container">
+      <textarea
+        id="editor"
+        value={markdown}
+        onChange={handleChange}
+      />
+      <div id="preview">
+        <ReactMarkdown children={markdown} />
       </div>
-   )
+    </div>
+  );
 }
 
-export default App
+// Add custom CSS. watch that one yt video
+// Make webpage responsive
